6960a10136b0abefd02ac1a0b6dc3f4fb05ad8a0
Rav Sohal <ravinder.sohal@gmail.com>
2019-12-09 01:21:53 +0000

    Playlist view unit test errors fixed

--- src/app/views/playlist-view/playlist-view/playlist-view.component.spec.ts
+++ src/app/views/playlist-view/playlist-view/playlist-view.component.spec.ts
@@ -2,7 +2,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from '../../../components/header/header/header.component';
 import { FooterComponent } from '../../../components/footer/footer/footer.component';
 import { PlaylistViewComponent } from './playlist-view.component';
-
+import { NgxPaginationModule } from 'ngx-pagination';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('PlaylistViewComponent', () => {
   let component: PlaylistViewComponent;
@@ -12,6 +14,9 @@ describe('PlaylistViewComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ PlaylistViewComponent, HeaderComponent, FooterComponent], 
       imports: [
+        NgxPaginationModule, 
+        FontAwesomeModule,
+        RouterTestingModule
       ],
     })
     .compileComponents();
